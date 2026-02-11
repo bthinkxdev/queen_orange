094e69ed5afceb4cbd734f8b2630724f3abd511d
bthinkxdev <bthinkx@gmail.com>
2026-02-11 20:39:11 +0530

    db updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -100,11 +100,11 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'queen_orange_db',
-        'USER': 'project_admin',
-        'PASSWORD': 'StrongPasswordHere',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
